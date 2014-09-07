60d3f87fc0e0077332c1b9f9226fe38c7e7013a0
Lech <lkhanzo87@gmail.com>
2014-09-07 14:02:25 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LimHouse::Application.config.secret_token = '26cc756b4fb868280516136da72c427ff49a352c6e9b6c800cc8dc332ce14ce5db4eca9b639d70dfd57da407d2f25fbaa1d87b7cf43c8d447d20b8fb5f23b9fc'
