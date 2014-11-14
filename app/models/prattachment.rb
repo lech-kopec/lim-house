72c870372f10dbaaf33a24e487567358f18aef9f
lech-kopec <lech.kopec@gmail.com>
2014-11-14 13:43:19 +0100

    add paperclip ftp storage

--- app/models/prattachment.rb
+++ app/models/prattachment.rb
@@ -3,7 +3,14 @@ class Prattachment < ActiveRecord::Base
 
   belongs_to :prodreturn
 
-  has_attached_file :pr_file, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "rails.png"
+  has_attached_file :pr_file, :storage=> :ftp, :path => "/lim-trade-reklamacje/:attachment/:id/:style/:filename", :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "rails.png",
+  					:ftp_servers => [
+										{
+										:host     => "fire.dhosting.pl",
+										:user     => "limtrade",
+										:password => "Kwidzyn87800"
+										}
+									]
   validates_attachment :pr_file, content_type: { content_type: ["application/pdf", /\Aimage\/.*\Z/] }
 
 end
